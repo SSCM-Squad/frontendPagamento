2df7dcb77951f4553a00a5ce6298ec961ea5300b
leafarpontes <rafapontes2008@hotmail.com>
2022-04-22 15:25:07 -0300

    Removido comentários desnecessários

--- src/app/funcionarios/IFuncionarios.ts
+++ src/app/funcionarios/IFuncionarios.ts
@@ -10,16 +10,3 @@ export interface IFuncionarios {
   ajudaDeCusto: number,
   valorPlanoDeSaude: number
 }
-
-// export class IFuncionarios {
-//   id!: number;
-//   nome!: string;
-//   salario!: number;
-//   funcao!: string;
-//   adicionalPericulosidade!: number;
-//   jornadaDeTrabalho!: number;
-//   horasExtras!: number;
-//   dependentes!: number;
-//   ajudaDeCusto!: number;
-//   valorPlanoDeSaude!: number;
-// }
